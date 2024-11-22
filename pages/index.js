21d54de92a208a5ff4ef51ca460d97635d540760
Rayane Stefanie <97451324+RayaneStefanie@users.noreply.github.com>
2024-11-22 23:46:52 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Mãezinha, eu te amo muito, se você também me ama, me dá um abraço!</h1>
+  return (
+    <h1>Mãezinha, eu te amo muito, se você também me ama, me dá um abraço!</h1>
+  );
 }
 
 export default Home;
