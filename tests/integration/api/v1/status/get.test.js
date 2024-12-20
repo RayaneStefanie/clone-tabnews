2bf5f7ac9ece78274efca5daf99a6b072af10048
Rayane Stefanie <97451324+RayaneStefanie@users.noreply.github.com>
2024-12-20 23:11:18 +0000

    adds `/api/v1/migrations` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,9 +4,8 @@ test("GET to /api/v1/status shoud return 200", async () => {
 
   const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(
-    expect(responseBody.updated_at).toEqual(parsedUpdatedAt),
-  );
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
